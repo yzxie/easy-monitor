6b9f1182ca3198a9235f4dc5c0551bdaef7d3486
xieyizun <xieyizun@tigerbrokers.com>
2018-11-05 23:37:53 +0800

    init project

---
+++ monitor-starter/src/main/java/com/yzxie/easy/monitor/starter/MainServer.java
@@ -0,0 +1,12 @@
+package com.yzxie.easy.monitor.starter;
+
+/**
+ * @author xieyizun
+ * @date 5/11/2018 23:37
+ * @description:
+ */
+public class MainServer {
+    public static void main(String[] args) {
+        
+    }
+}
